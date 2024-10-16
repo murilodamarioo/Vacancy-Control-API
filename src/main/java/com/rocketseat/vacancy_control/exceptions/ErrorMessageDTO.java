8e2792547f0714f875025fc98174267e540e43fd
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-15 22:19:36 -0300

    feat: implement Exceptions for check fields

---
+++ src/main/java/com/rocketseat/vacancy_control/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package com.rocketseat.vacancy_control.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+  
+  private String message;
+  private String field;
+}
