eef295b229870921dbf351dd8053abdce0891acb
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-20 20:41:43 -0300

    feat: hash cadidate password

--- src/main/java/com/rocketseat/vacancy_control/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/rocketseat/vacancy_control/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package com.rocketseat.vacancy_control.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rocketseat.vacancy_control.exceptions.UserFoundException;
@@ -12,12 +13,19 @@ public class CreateCandidateUseCase {
   @Autowired
   private CandidateRepository candidateRepository;
 
-  public CandidateEntity execute(CandidateEntity candidateEntity) {
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  public CandidateEntity execute(CandidateEntity candidateEntity) { 
     this.candidateRepository
     .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
     .ifPresent((user) -> {
       throw new UserFoundException();
     });
+
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
+
     return this.candidateRepository.save(candidateEntity);
   }
 }
