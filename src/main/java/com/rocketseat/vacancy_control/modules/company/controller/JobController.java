6bb17a4cf22625002c64bc2f54d26690ee02e513
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-20 20:19:07 -0300

    refactor: jobContorller and create DTO for JobEntity

--- src/main/java/com/rocketseat/vacancy_control/modules/company/controller/JobController.java
+++ src/main/java/com/rocketseat/vacancy_control/modules/company/controller/JobController.java
@@ -1,15 +1,20 @@
 package com.rocketseat.vacancy_control.modules.company.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rocketseat.vacancy_control.modules.company.dto.CreateJobDTO;
 import com.rocketseat.vacancy_control.modules.company.entites.JobEntity;
 import com.rocketseat.vacancy_control.modules.company.useCases.CreateJobUseCase;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -20,8 +25,18 @@ public class JobController {
   private CreateJobUseCase createJobUseCase;
   
   @PostMapping("/")
-  public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
-    var response =this.createJobUseCase.execute(jobEntity);
-    return ResponseEntity.status(201).body(response);
+  public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+    var companyId = request.getAttribute("company_id");
+
+    var jobEntity = JobEntity.builder()
+    .benefits(createJobDTO.getBenefits())
+    .companyId(UUID.fromString(companyId.toString()))
+    .description(createJobDTO.getDescription())
+    .level(createJobDTO.getLevel())
+    .build();
+    
+    var response = this.createJobUseCase.execute(jobEntity);
+    
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 }
---
+++ src/main/java/com/rocketseat/vacancy_control/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,12 @@
+package com.rocketseat.vacancy_control.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+  private String description;
+  private String benefits;
+  private String level;
+
+}
