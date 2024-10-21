a7cedf61a5e14047c66b17bb6c153fb9ee64dae6
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-20 22:57:36 -0300

    feat: create candidate token

---
+++ src/main/java/com/rocketseat/vacancy_control/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,5 @@
+package com.rocketseat.vacancy_control.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+
+}
---
+++ src/main/java/com/rocketseat/vacancy_control/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package com.rocketseat.vacancy_control.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthCandidateResponseDTO {
+
+  private String access_token;
+}
