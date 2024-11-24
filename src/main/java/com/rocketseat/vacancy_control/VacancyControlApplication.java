c4a1272fdd52dda1792d06f5293a36c8d116880c
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-24 15:52:32 -0300

    feat: Config swagger

--- src/main/java/com/rocketseat/vacancy_control/VacancyControlApplication.java
+++ src/main/java/com/rocketseat/vacancy_control/VacancyControlApplication.java
@@ -1,9 +1,18 @@
 package com.rocketseat.vacancy_control;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Vacancy Control",
+				description = "API responsible for vacancy control",
+				version = "1"
+		)
+)
 public class VacancyControlApplication {
 
 	public static void main(String[] args) {
