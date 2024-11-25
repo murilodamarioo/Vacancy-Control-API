787212769d23d679151b6afe325cc505bdb637fb
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-24 22:40:33 -0300

    feat: create company documentation

--- src/main/java/com/rocketseat/vacancy_control/modules/company/controller/AuthCompanyController.java
+++ src/main/java/com/rocketseat/vacancy_control/modules/company/controller/AuthCompanyController.java
@@ -1,5 +1,11 @@
 package com.rocketseat.vacancy_control.modules.company.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +19,24 @@ import com.rocketseat.vacancy_control.modules.company.useCases.AuthCompanyUseCas
 
 @RestController
 @RequestMapping("/company")
+@Tag(name = "Company", description = "Company information")
 public class AuthComapanyController {
 
   @Autowired
   private AuthCompanyUseCase authCompanyUseCase;
   
   @PostMapping("/auth")
+  @Operation(summary = "Company authentication")
+  @ApiResponses({
+          @ApiResponse(responseCode = "200", content = {
+                  @Content(schema = @Schema(implementation = AuthCompanyDTO.class))
+          }),
+          @ApiResponse(responseCode = "401", description = "Username/password incorrect")
+  })
   public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
     try {
       var response = this.authCompanyUseCase.execute(authCompanyDTO);
-      return ResponseEntity.status(HttpStatus.CREATED).body(response);
+      return ResponseEntity.status(HttpStatus.OK).body(response);
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
