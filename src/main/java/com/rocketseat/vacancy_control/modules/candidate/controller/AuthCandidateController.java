26c557b91161f7f33c83179a119c0ea2423da912
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-24 21:53:48 -0300

    feat: Auth Candidate controller documentation

--- src/main/java/com/rocketseat/vacancy_control/modules/candidate/controller/AuthCandidateController.java
+++ src/main/java/com/rocketseat/vacancy_control/modules/candidate/controller/AuthCandidateController.java
@@ -1,5 +1,11 @@
 package com.rocketseat.vacancy_control.modules.candidate.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +19,20 @@ import com.rocketseat.vacancy_control.modules.candidate.useCases.AuthCandidateUs
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidate", description = "Candidate information")
 public class AuthCandidateController {
   
   @Autowired
   private AuthCandidateUseCase authCandidateUseCase;
 
   @PostMapping("/auth")
+  @Operation(summary = "Candidate authentication")
+  @ApiResponses({
+          @ApiResponse(responseCode = "200", content = {
+                  @Content(schema = @Schema(implementation = AuthCandidateRequestDTO.class))
+          }),
+          @ApiResponse(responseCode = "401", description = "Username/password incorrect")
+  })
   public ResponseEntity<Object> auth(@RequestBody AuthCandidateRequestDTO authCandidateRequestDTO) {
     try {
       var response = this.authCandidateUseCase.execute(authCandidateRequestDTO);
