21c63b4c4ceb58a45091d12e49fa828727ea4bb8
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-28 22:21:45 -0300

    feat: create apply job entity

---
+++ src/main/java/com/rocketseat/vacancy_control/modules/candidate/entity/ApplyJobEntity.java
@@ -0,0 +1,39 @@
+package com.rocketseat.vacancy_control.modules.candidate.entity;
+
+import com.rocketseat.vacancy_control.modules.company.entity.JobEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "apply_jobs")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplyJobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "candida_id", insertable = false, updatable = false)
+    private CandidateEntity candidateEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id", insertable = false, updatable = false)
+    private JobEntity jobEntity;
+
+    @Column(name = "candidate_id")
+    private UUID candidateId;
+
+    @Column(name = "job_id")
+    private UUID jobId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
