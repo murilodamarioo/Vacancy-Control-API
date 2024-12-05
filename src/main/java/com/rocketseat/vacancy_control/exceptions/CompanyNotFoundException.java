b267e8019ddac751d76deec5a022a0511ac9414c
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-04 21:54:11 -0300

    feat: create company not found exception class

---
+++ src/main/java/com/rocketseat/vacancy_control/exceptions/CompanyNotFoundException.java
@@ -0,0 +1,8 @@
+package com.rocketseat.vacancy_control.exceptions;
+
+public class CompanyNotFoundException extends RuntimeException {
+  
+  public CompanyNotFoundException() {
+    super("Company not found");
+  }
+}
